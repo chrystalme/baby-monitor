00270ab76454dcbf307196f9ea54e16b4f78e920
chrystalme <afam_ifeanyi@live.com>
2021-09-02 16:02:36 +0300

    Add actions loginUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,40 +1,40 @@
-export const LOGIN_REQUEST = 'LOGIN_REQUEST'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const requestLogin = (creds) => ({
   type: LOGIN_REQUEST,
   isFetching: true,
   isAuthenticated: false,
   payload: creds,
-})
+});
 
 export const receiveLogin = (user) => ({
   type: LOGIN_SUCCESS,
   isFetching: false,
   isAuthenticated: true,
   id_token: user.id_token,
-})
+});
 
 export const loginError = (message) => ({
   type: LOGIN_FAILURE,
   isFetching: false,
   isAuthenticated: false,
   message,
-})
+});
 
-export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
-export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 
 export const requestLogout = () => ({
   type: LOGOUT_REQUEST,
   isFetching: true,
   isAuthenticated: true,
-})
+});
 
 export const receiveLogout = () => ({
   type: LOGOUT_SUCCESS,
   isFetching: false,
   isAuthenticated: false,
-})
+});
